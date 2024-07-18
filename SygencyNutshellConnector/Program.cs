c4d728a58bbbb5021f052a933aba55c20d5ef278
Michael Morales <mmorales@clearfuze.com>
2024-07-17 19:33:01 -0700

    app

--- SygencyNutshellConnector/Program.cs
+++ SygencyNutshellConnector/Program.cs
@@ -16,15 +16,17 @@ namespace SygencyNutshellConnector
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
-
             builder.WebHost.ConfigureKestrel(opts =>
             {
                 opts.Listen(IPAddress.Loopback, port: 80); // listen on http://localhost:5002
                 opts.ListenAnyIP(80); // listen on http://*:5003
                 opts.ListenLocalhost(80, listenOptions => listenOptions.UseHttps());  // listen on https://localhost:5004
-      
+
             });
+
+            var app = builder.Build();
+
+
             app.UseSwagger();
                 app.UseSwaggerUI();
 
